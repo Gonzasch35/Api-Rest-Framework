a3b83251b392d3e5fcd078f10fae7520a7286e0b
Gonzalo Schmidt <gonza.sch35@gmail.com>
2024-04-20 21:13:38 -0300

    migrations

--- food/models.py
+++ food/models.py
@@ -11,9 +11,9 @@ class Category(models.Model):
 
 class Food(models.Model):
     name = models.CharField(max_length=200)
-    option_1 = models.CharField(models.CharField(max_length=100))
-    option_2 = models.CharField(models.CharField(max_length=100))
-    option_3 = models.CharField(models.CharField(max_length=100))
+    option_1 = models.CharField(max_length=100, default=None)
+    option_2 = models.CharField(max_length=100, default=None)
+    option_3 = models.CharField(max_length=100, default=None)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
